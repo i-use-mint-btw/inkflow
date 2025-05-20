4b540e8d8a4a2acecf8aa681f85d3e5e11204875
Joel Gill <joelgill780@gmail.com>
2025-05-20 02:06:07 -0400

    Fixed typescript compilation error in EditorWindow.vue

--- frontend/src/components/EditorWindow.vue
+++ frontend/src/components/EditorWindow.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { getTextColor } from '../utils';
-
+import { getTextColor } from "../utils";
 
 defineProps({
-  modelValue: String
-})
+  modelValue: String,
+});
 
-const emit = defineEmits(['update:modelValue'])
-
+const emit = defineEmits(["update:modelValue"]);
 </script>
 
-
 <template>
-  <section class="w-1/2 h-full bg-transparent border-r-2 border-gray-400 overflow-hidden">
-    <textarea class="focus:outline-none w-full h-full p-4 resize-none" name="Markdown editor" :style="{color: getTextColor()}" :value="modelValue" @input="emit('update:modelValue', $event.target?.value)"></textarea>
+  <section
+    class="w-1/2 h-full bg-transparent border-r-2 border-gray-400 overflow-hidden"
+  >
+    <textarea
+      class="focus:outline-none w-full h-full p-4 resize-none"
+      name="Markdown editor"
+      :style="{ color: getTextColor() }"
+      :value="modelValue"
+      @input="(e: any) => emit('update:modelValue', e.target.value)"
+    ></textarea>
   </section>
 </template>
