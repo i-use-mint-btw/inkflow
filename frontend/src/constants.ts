b2484a98171be89b8df45b94cae20e14b14feef8
Joel Gill <joelgill780@gmail.com>
2025-05-21 16:21:41 -0400

    Added proper backend urls for prod and development

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
 //export const API_URL = "https://backend-production-cd54.up.railway.app"
-export const API_URL = "http://localhost:2680"
+export const API_URL = process.env.NODE_ENV  === "development" ? "http://localhost:2680" : "https://backend-production-cd54.up.railway.app"
