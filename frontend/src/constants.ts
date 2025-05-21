148d463b933198f33edbb1983b91c7e3e444026f
Joel Gill <joelgill780@gmail.com>
2025-05-21 16:20:44 -0400

    Changed some configuratios

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export const API_URL = "https://backend-production-cd54.up.railway.app"
+//export const API_URL = "https://backend-production-cd54.up.railway.app"
+export const API_URL = "http://localhost:2680"
